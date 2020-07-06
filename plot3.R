0037176df0d6236b7133e079702414fdb6d00bff
Uzma <62062063+uzma35@users.noreply.github.com>
2020-07-06 22:23:28 +0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,3 +1,14 @@
+##loading the electricity consumption dataset
+
+power_con <- read.table("household_power_consumption.txt",skip=1,sep=";")
+
+##naming the dataset
+
+names(power_con) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+##subsetting the dataset for desired dates
+
+subpower_con <- subset(power_con,power_con$Date=="1/2/2007" | power_con$Date =="2/2/2007")
 
 ##basic plot 
 
